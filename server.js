70636f48c76d9a7160d30ff7374e3ff6d35f827e
owalkz <jjaraorie@gmail.com>
2024-10-04 16:11:32 +0300

    Modified cors options

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 15000;
 const cors = require("cors");
 const connect = require("./config/db");
 const errorHandler = require("./middleware/errorMiddleware");
@@ -9,7 +9,7 @@ connect();
 
 // CORS configuration
 const corsOptions = {
-  origin: "http://127.0.0.1:8080",
+  origin: process.env.FRONTEND_URL,
   methods: "GET, PUT, POST, DELETE",
 };
 app.listen(port, () => {
